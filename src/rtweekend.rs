43ffd8bbed9899e3023eae8a1c27d54157296a6d
Erik Karlgren Domercq <erik.karlgren@gmail.com>
2022-09-20 22:56:17 +0200

    Added rand crate

--- src/rtweekend.rs
+++ src/rtweekend.rs
@@ -1,3 +1,6 @@
+use rand::{thread_rng, Rng};
+use std::ops::Range;
+
 // Constants
 pub const PI: f64 = std::f64::consts::PI;
 
@@ -8,4 +11,8 @@ pub fn degrees_to_radians(degrees: f64) -> f64 {
 
 pub fn radians_to_degrees(radians: f64) -> f64 {
     PI / (180.0 * radians)
+}
+
+pub fn random_num(range: Range<f64>) -> f64 {
+    thread_rng().gen_range(range)
 }
