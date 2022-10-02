3b9729e9ac82a986d4ab2e7d0bcb9cb9796874bd
Erik Karlgren Domercq <erik.karlgren@gmail.com>
2022-10-02 16:39:54 +0200

    Minor changes in scripts

--- scripts/run_and_show_image.sh
+++ scripts/run_and_show_image.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Compiling..."
 cargo build --release
+#cargo build 
 echo "Running..."
-time target/release/ray-tracing-in-one-weekend >example.ppm
+time target/release/ray_tracing_in_one_weekend >example.ppm
 echo "Showing image..."
 gwenview example.ppm 2>/dev/null
--- scripts/run_flamegraph.sh
+++ scripts/run_flamegraph.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "Compiling..."
+cargo build --release
 echo "Running 'cargo flamegraph'..."
-cargo flamegraph >example.ppm 2>/dev/null | head -n-1
+time cargo flamegraph >example.ppm 2>/dev/null | head -n-1
 echo "Opening 'flamegraph.svg' in firefox..."
 firefox flamegraph.svg 2>/dev/null
